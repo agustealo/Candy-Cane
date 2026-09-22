2283605cf98b37c9a88b58aeb0aa027cba2b8f0f
Agustealo Johnson <agustealo@gmail.com>
2026-09-22 11:14:29 -0400

    Modernize page loop without changing layout

--- loop-page.php
+++ loop-page.php
@@ -1,36 +1,28 @@
-<!-- Start the Loop -->
+<?php
+/**
+ * Page loop template.
+ *
+ * @package Candy_Cane
+ */
 
-	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-<div class="two columns">	
-
+if ( have_posts() ) :
+	while ( have_posts() ) :
+		the_post();
+		?>
+		<div class="two columns">
 			<h2 class="potitle"><?php the_title(); ?></h2>
+		</div>
 
-</div>
-
-<div class="seven columns">
-	
-		<!-- Begin the first div -->
-		<div>
-				
-			
-			<!-- Display the Page's Content in a div box. -->
+		<div class="seven columns">
 			<div class="entry">
 				<?php the_content(); ?>
 			</div>
-		
+			<?php wp_link_pages(); ?>
 		</div>
-		<!-- Closes the first div -->
-	
-	<!-- Stop The Loop (but note the "else:" - see next line). -->
-	<?php endwhile; else: ?>
-	
-		<!-- The very first "if" tested to see if there were any Posts to -->
-		<!-- display.  This "else" part tells what do if there weren't any. -->
-
-		<div class="alert-box error"><?php _e('Sorry, the page you requested was not found', 'four') ?> </div>
-	
-	<!--End the loop -->
-	<?php endif; ?>
-	
-</div>
+		<?php
+	endwhile;
+else :
+	?>
+	<div class="alert-box error"><?php esc_html_e( 'Sorry, the page you requested was not found', 'candy-cane' ); ?></div>
+	<?php
+endif;
