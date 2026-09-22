db7bb80ba930344fff7c771cbae2435d43a92ddc
Agustealo Johnson <agustealo@gmail.com>
2026-09-22 10:57:08 -0400

    Modernize document head without changing theme layout

--- header.php
+++ header.php
@@ -1,63 +1,43 @@
-<!DOCTYPE html>
-
-<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
-<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>> <![endif]-->
-<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]-->
-<!--[if IE 8]>    <html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
-<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?>> <!--<![endif]-->
+<!doctype html>
+<html <?php language_attributes(); ?>>
 <head>
-	<meta charset="utf-8" />
-	
-	<!-- Set the viewport width to device width for mobile -->
-    <meta name="viewport" content="initial-scale=1.6; maximum-scale=1.0; width=device-width; "/>
-	
-	<title><?php bloginfo('name'); ?> | <?php is_home() ? bloginfo('description') : wp_title(''); ?></title>
-  
-	<!-- Included CSS Files -->
-	<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
-	
-	<!-- IE Fix for HTML5 Tags -->
-	<!--[if lt IE 9]>
-		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]-->
-
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
-<?php if (function_exists('wp_enqueue_script') && function_exists('is_singular')) : ?>
-<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
-<?php endif; ?>
-<?php wp_head(); ?>
-
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<?php wp_head(); ?>
 </head>
-
 <body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
 
-	<!-- Begin Container -->
-	<div class="container">
-		
-		<!-- Header Row -->
-		<div class="row">
-			
-				<!-- Header Column -->
-				<div class="two columns" id="access" role="navigation">
-					
-				
-					<!-- Site Description & Title -->
-					<hgroup id="header">
-						<h1><a href="<?php echo site_url(); ?>"><?php bloginfo('title'); ?></a></h1>
-					</hgroup>
+<div class="container">
+	<div class="row">
+		<div class="two columns" id="site-branding">
+			<header id="header">
+				<h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+			</header>
 		</div>
-					<!-- Navigation --> 	
-				<div class="ten columns fixie" id="access" role="navigation">
-        <?php wp_nav_menu( array( 'theme_location' => 'header-menu2','menu_class' => 'nav-bar2','depth' => 1,'container' => 'nav') ); ?>				
- <?php wp_nav_menu( array( 'theme_location' => 'header-menu1','menu_class' => 'nav-bar','depth' => 1,'container' => 'nav') ); ?>
-	 		
-				</div>
-				<!-- Header Column -->
-				
-        </div>
 
-		<!-- Header Row -->
-		
-		<!-- Main Row -->
+		<div class="ten columns fixie" id="access" role="navigation" aria-label="<?php esc_attr_e( 'Primary navigation', 'candy-cane' ); ?>">
+			<?php
+			wp_nav_menu(
+				array(
+					'theme_location' => 'header-menu2',
+					'menu_class'     => 'nav-bar2',
+					'depth'          => 1,
+					'container'      => 'nav',
+					'fallback_cb'    => false,
+				)
+			);
+			wp_nav_menu(
+				array(
+					'theme_location' => 'header-menu1',
+					'menu_class'     => 'nav-bar',
+					'depth'          => 1,
+					'container'      => 'nav',
+					'fallback_cb'    => false,
+				)
+			);
+			?>
+		</div>
+	</div>
+
 	<div class="row wrap">
-
