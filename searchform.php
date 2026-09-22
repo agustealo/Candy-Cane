ce4089aad95b35c539113d4739baf39e8d5276ee
Agustealo Johnson <agustealo@gmail.com>
2026-09-22 11:14:43 -0400

    Modernize accessible search form markup

--- searchform.php
+++ searchform.php
@@ -1,5 +1,15 @@
-<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-    <div><label class="screen-reader-text" for="s"><?php _e('Search For:', 'four') ?></label>
-        <input type="search" results="5" placeholder="Search" name="s" id="s" />
-    </div>
+<?php
+/**
+ * Search form template.
+ *
+ * @package Candy_Cane
+ */
+
+$candy_cane_search_field_id = wp_unique_id( 'candy-cane-search-field-' );
+?>
+<form role="search" method="get" id="searchform" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+	<div>
+		<label class="screen-reader-text" for="<?php echo esc_attr( $candy_cane_search_field_id ); ?>"><?php esc_html_e( 'Search for:', 'candy-cane' ); ?></label>
+		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search', 'search form placeholder', 'candy-cane' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" id="<?php echo esc_attr( $candy_cane_search_field_id ); ?>" />
+	</div>
 </form>
