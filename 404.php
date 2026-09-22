0da4ab6d3c3f4faa1addac63823a9449a298a93f
Agustealo Johnson <agustealo@gmail.com>
2026-09-22 11:14:35 -0400

    Modernize 404 template escaping and text domain

--- 404.php
+++ 404.php
@@ -1,16 +1,23 @@
-<?php get_header(); ?>
-	
-	<div class="nine columns">
-	<div class="alert-box error"><?php _e('Just an error.404.', 'four') ?></div>
-	<h2><?php _e('Not Found', 'four') ?></h2>
-	<p><?php _e('Try to search the site', 'four') ?></p>
-	
+<?php
+/**
+ * 404 template.
+ *
+ * @package Candy_Cane
+ */
+
+get_header();
+?>
+
+<div class="nine columns">
+	<div class="alert-box error"><?php esc_html_e( 'Just an error.404.', 'candy-cane' ); ?></div>
+	<h2><?php esc_html_e( 'Not Found', 'candy-cane' ); ?></h2>
+	<p><?php esc_html_e( 'Try to search the site', 'candy-cane' ); ?></p>
+
 	<?php get_search_form(); ?>
-	
-	<a href="<?php echo home_url( '/' ); ?>"><?php _e('Go Home', 'four') ?></a>
 
-        </div>
+	<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php esc_html_e( 'Go Home', 'candy-cane' ); ?></a>
+</div>
 
-<?php get_sidebar(); ?>
-		
-<?php get_footer(); ?>
+<?php
+get_sidebar();
+get_footer();
