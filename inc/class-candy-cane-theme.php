67b3ef7c5b8c9a1f106c9a60406e0c7bc9ba3b13
Agustealo Johnson <agustealo@gmail.com>
2026-09-22 16:11:20 -0400

    Add preservation-safe editor styling support

--- inc/class-candy-cane-theme.php
+++ inc/class-candy-cane-theme.php
@@ -39,6 +39,8 @@ final class Candy_Cane_Theme {
 		add_theme_support( 'post-thumbnails' );
 		add_theme_support( 'responsive-embeds' );
 		add_theme_support( 'wp-block-styles' );
+		add_theme_support( 'editor-styles' );
+		add_editor_style( 'stylesheets/editor.css' );
 		add_theme_support(
 			'html5',
 			array(
